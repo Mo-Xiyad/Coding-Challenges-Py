8dc28a44a408efa68f5b64859dd446395edbf880
Mohamed Xiyad <76219804+Mo-Xiyad@users.noreply.github.com>
2021-04-13 00:08:56 +0200

    Update Arithmetic_Operators.py

--- Arithmetic_Operators.py
+++ Arithmetic_Operators.py
@@ -53,10 +53,4 @@ print(diff(a, b))
 def product(a, b):
     return a * b
 
-
-# print(product(a, b))
-
-# a = int(input())
-# b = int(input())
-
 print('{0} \n{1} \n{2}'.format((a + b), (a - b), (a * b)))
